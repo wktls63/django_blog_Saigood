7a618a91b7fe290294bc3be74532901de93bd5e0
kyungmin <wktls63@naver.com>
2023-09-19 08:09:14 +0900

    fix urls

--- Saigoodblog/blog/urls.py
+++ Saigoodblog/blog/urls.py
@@ -11,14 +11,13 @@ router.register(r"articles", ArticleViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     
-    path("post/<int:article_id>", views.post_detail, name="post"),
+    path("post/<int:article_id>", post_detail, name="post"),
     path("signup/", SignUpView.as_view(), name="sign_up"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", logout_view, name="logout"),
 
     path('board/', article_list, name='board'),
     path('board/<str:topic>', article_list, name='board_by_topic'),
-    path("post/<int:article_id>", post_detail, name="post"),
 
     path('write/', write, name='write'),
 ]
