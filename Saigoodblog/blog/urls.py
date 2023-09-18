dba2786f64f3fb1fd7b0042b8046a704c5adfef7
jhlee <makk7201@gmail.com>
2023-09-18 16:31:30 +0900

    Fix urls.py 'board/'
    - 'board/' 주소로 접속하면 article_list 함수 실행되도록 경로 수정
    - 'board/주제' 주소로 들어오면 주제에 맞춰 필터링된 목록만 보여주도록 수정

--- Saigoodblog/blog/urls.py
+++ Saigoodblog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ArticleViewSet
-from .views import board, write
+from .views import board, write, article_list
 
 from .views import SignUpView, posting, logout_view, LoginView
 
@@ -12,10 +12,13 @@ urlpatterns = [
     path('api/', include(router.urls)),
     
     path("post/", posting, name="posting"),
+
     path("signup/", SignUpView.as_view(), name="sign_up"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", logout_view, name="logout"),
 
-    path('board/', board, name='board'),
+    path('board/', article_list, name='board'),
+    path('board/<str:topic>', article_list, name='board_by_topic'),
+
     path('write/', write, name='write'),
 ]
