d5b900822aa28be8d5f6474746d8f640ff364291
sujin1130 <tnwlsdpswpf@naver.com>
2023-09-19 15:39:35 +0900

    Fix urls.py
    앱 레벨 url 이름 동일한 부분 통일
    
    by. 박수진

--- Saigoodblog/blog/urls.py
+++ Saigoodblog/blog/urls.py
@@ -11,16 +11,13 @@ router.register(r"articles", ArticleViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     
-    path("post/<int:article_id>", post_detail, name="post"),
     path("signup/", SignUpView.as_view(), name="sign_up"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", logout_view, name="logout"),
 
-
-    path('board/', article_list, name='board'),
+    path('', article_list, name='board'),
+    path('board/<str:topic>', article_list, name='board_by_topic'),
+    path("post/<int:article_id>", post_detail, name="posting"),
     path('write/', create_or_update_post, name='create_or_update_post'),
     path('edit_post/<int:post_id>/', create_or_update_post, name='create_or_update_post'),
-    path('board/<str:topic>', article_list, name='board_by_topic'),
-
-    path('write/', write, name='write'),
 ]
